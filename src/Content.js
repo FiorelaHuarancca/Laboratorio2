dc69c7321f4618cdcc42335f198d9c8a92c62e8d
Jeydi Fiorela Huarancca Pacheco <jeydi.huarancca@tecsup.edu.pe>
2021-03-26 23:45:43 -0500

    Tercer ejercicio

---
+++ src/Content.js
@@ -0,0 +1,12 @@
+const Content = (props) => {
+    return(
+        <h2>
+            <p> {props.part1} {props.exercises1} </p>
+            <p> {props.part2} {props.exercises2} </p>
+            <p> {props.part3} {props.exercises3} </p>
+        </h2>
+        
+    )
+}
+
+export default Content
