944e9b635f455e497c562e0ff9f44b7e1db2079c
Jeydi Fiorela Huarancca Pacheco <jeydi.huarancca@tecsup.edu.pe>
2021-03-26 23:57:35 -0500

    Cuarto ejercicio

--- src/Content.js
+++ src/Content.js
@@ -1,10 +1,13 @@
 const Content = (props) => {
     return(
         <h2>
-            <p> {props.part1} {props.exercises1} </p>
-            <p> {props.part2} {props.exercises2} </p>
-            <p> {props.part3} {props.exercises3} </p>
+            <Part part={props.part1} />
+            <Part part={props.part2} />
+            <Part part={props.part3} />
         </h2>
+    
+            
+
         
     )
 }
---
+++ src/Part.js
@@ -0,0 +1,11 @@
+const Part = (props) => {
+    return(
+        <h3>
+            <p>{props.part.nombre}</p>
+            <p>{props.part.exercises}</p>
+        </h3>
+        
+    )
+}
+
+export default Total
